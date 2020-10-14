237ebd73ecaf9d5ea90e3bdfd1fe4ab019b72aa1
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-13 20:20:02 -0500

    Both generators run as expected
    Fixed a typo and error in genTrips()

--- database/seed-10m.js
+++ database/seed-10m.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 
 // This const determines the number of records generated
-const RECORDS_QUANTITY = 1000;
+const RECORDS_QUANTITY = 10;
 
 // Generates location data
 const genLocations = () => {
@@ -9,13 +9,13 @@ const genLocations = () => {
   for (let i = 0; i < RECORDS_QUANTITY; i++) {
     const lowDays = [];
     const today = new Date();
-    for (var j = 0; j < 40: j++) {
+    for (var j = 0; j < 40; j++) {
       lowDays.push(new Date(faker.date.future(0.5, today)))
     }
     const location = {
       id: i,
       rooms: Math.floor(Math.random() * 20) + 5,
-      name: `hotel${i}`,
+      name: faker.name.lasName(),
       lowDays: lowDays
     };
     locations.push(location);
@@ -27,13 +27,14 @@ const genLocations = () => {
 const genTrips = () => {
   const trips = [];
   for (let i = 0; i < RECORDS_QUANTITY; i++) {
-    const numberOfResevervations = Math.floor(Math.random() * 30);
+    const numberOfResevervations = Math.floor(Math.random() * 25);
     const adults = Math.ceil(Math.random() * 10);
+    const checkIn = faker.date.soon(90);
     for (let j = 0; j < numberOfResevervations; j++) {
       let trip = {
         locationId: i,
-        checkIn: faker.date.soon(90),
-        checkOut: faker.date.future(0.12, trip.checkIn),
+        checkIn: checkIn,
+        checkOut: faker.date.future(0.12, checkIn),
         adults: adults,
         children: Math.floor(Math.random() * 10),
         rooms: Math.ceil(Math.random() * adults),
