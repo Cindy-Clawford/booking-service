961c99ae1de476d899a71ea2d18225096089fd1f
Charlie Sellers <CharlieJSellers@gmail.com>
2020-10-19 13:25:17 -0500

    Now implements streams for data storage
    Next, I will be attempting to implement workers

--- database/genRecords.js
+++ database/genRecords.js
@@ -1,60 +1,93 @@
 const faker = require('faker');
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+let writer = csvWriter();
+const targetRecords = 10000000;
 
 // Generates location data in tuples
-// 0: rooms, 1: name
-const genLocations = (blockSize) => {
-  const locations = [];
-  for (let i = 0; i < blockSize; i++) {
-
-    const location = [
-      Math.floor(Math.random() * 20) + 5,
-      faker.name.lastName()
-    ];
-    locations.push(location);
+const genLocations = () => {
+  writer.pipe(fs.createWriteStream('locations.csv'));
+  for (let i = 0; i < targetRecords; i++) {
+    const count = i;
+    writer.write({
+      id: count,
+      rooms: Math.floor(Math.random() * 20) + 5,
+      name: faker.name.lastName()
+    })
   }
-  return locations;
 };
 
-// Generates lowDays
-// 0-9: date, 10: locationid
-const genLowDays = (blockSize, startingIndex) => {
-  const lowDays = [];
-  for (let i = startingIndex; i < startingIndex + blockSize; i++) {
-    const quantLowDays = Math.floor(Math.random() * 30);
-    for (var j = 0; j < quantLowDays; j++) {
-      lowDays.push([
-          new Date(faker.date.future(0.5, new Date())),
-          i
-      ]);
-    }
+const update = (primaryCount) => {
+  if(primaryCount % 10000 === 0) {
+    console.log(`Writing ${primaryCount} of ${targetRecords}`);
   }
-  return lowDays;
 }
 
-// Generates trip data
-const genTrips = (blockSize) => {
-  console.log(`Generating ${blockSize} trips`);
+// Generates lowDays
+const genLowDays = () => {
+  const today = new Date();
+  writer.pipe(fs.createWriteStream('lowdays.csv'));
 
-  const trips = [];
-  for (let i = 0; i < blockSize; i++) {
-    const numberOfResevervations = Math.floor(Math.random() * 25);
-    const adults = Math.ceil(Math.random() * 10);
-    const checkIn = faker.date.soon(90);
-    for (let j = 0; j < numberOfResevervations; j++) {
-      let trip = {
-        locationId: i,
-        checkIn: checkIn,
-        checkOut: faker.date.future(0.12, checkIn),
-        adults: adults,
-        children: Math.floor(Math.random() * 10),
-        rooms: Math.ceil(Math.random() * adults),
-      };
-      trips.push(trip);
+  const wrapper = (count = 0, primaryCount = 0) => {
+    let ok = true;
+
+    do {
+      primaryCount++;
+      update(primaryCount);
+      const quantLowDays = Math.floor(Math.random() * 10);
+      for (var j = 0; j < quantLowDays && ok === true; j++) {
+        ok = writer.write({
+            id: count++,
+            date: new Date(faker.date.future(0.5, today)),
+            locationid: primaryCount
+        });
+      }
+    } while (primaryCount < targetRecords && ok);
+
+    if (primaryCount > 0) {
+      writer.once('drain', wrapper.bind(this, count, primaryCount));
     }
-  }
-  return trips;
+  };
+
+  wrapper();
+}
+
+const genTrips = () => {
+  writer.pipe(fs.createWriteStream('trips.csv'));
+
+  const wrapper = (count = 0, primaryCount = 0) => {
+    let ok = true;
+    do {
+      primaryCount++;
+      update(primaryCount);
+
+      const numberOfResevervations = Math.floor(Math.random() * 25);
+      for (let j = 0; j < numberOfResevervations && ok; j++) {
+        const adults = Math.ceil(Math.random() * 10);
+        const checkIn = faker.date.soon(90);
+        ok = writer.write({
+          locationId: primaryCount,
+          checkIn: checkIn,
+          checkOut: faker.date.future(0.12, checkIn),
+          adults: adults,
+          children: Math.floor(Math.random() * 10),
+          rooms: Math.ceil(Math.random() * adults),
+        });
+      }
+    } while (primaryCount < targetRecords && ok);
+
+    if (primaryCount > 0) {
+      writer.once('drain', wrapper.bind(this, count, primaryCount));
+    }
+  };
+
+  wrapper();
 };
 
-module.exports = { genLocations, genTrips, genLowDays };
+// module.exports = { genLocations, genTrips, genLowDays };
 
-console.log(genLowDays(25, 1));
+// genLocations();
+// genLowDays();
+// genTrips();
